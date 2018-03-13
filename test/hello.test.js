d7d734fe6fa1ad50c195e0d7ea921590fbf1df04
uanoob <usnet@ukr.net>
2018-03-13 12:57:05 +0200

    Change test env

--- test/hello.test.js
+++
@@ -1,9 +0,0 @@
-const { expect } = require('chai');
-const request = require('request');
-
-it('Main page content', (done) => {
-  request('http://localhost:8080', (error, response, body) => {
-    expect(body).to.equal('Hello');
-    done();
-  });
-});
